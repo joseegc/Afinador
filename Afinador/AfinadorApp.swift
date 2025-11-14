1a07ac7c640a46b18d572c1a2e92b19136546e59
José Elias <joseelias5589@gmail.com>
2025-11-14 17:25:02 -0300

    Initial Commit

---
+++ Afinador/AfinadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfinadorApp.swift
+//  Afinador
+//
+//  Created by José Elias Gomes Camargo  on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AfinadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Afinador/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Afinador
+//
+//  Created by José Elias Gomes Camargo  on 14/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
